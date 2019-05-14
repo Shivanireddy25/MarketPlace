43ac9c3c9b4dc2c73feb10f72b21d5f9489ce233
Shivanireddy25 <shivanireddy.singireddy@sjsu.edu>
2019-05-14 14:30:59 -0700

    Styling

--- html/home.php
+++ html/home.php
@@ -30,7 +30,7 @@ if(!isset($_COOKIE[$cookie_name])) {
     <link
       rel="stylesheet"
       href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"
-      href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+     
     />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
