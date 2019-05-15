63949fd96ca604c43a47fef6c3952f7526f9cfe8
Shivanireddy25 <shivanireddy.singireddy@sjsu.edu>
2019-05-14 20:01:41 -0700

    Toprate

--- html/index.php
+++ html/index.php
@@ -116,8 +116,8 @@ echo "<script type = 'text/javascript'>window.localStorage.setItem('" . $id . "'
     <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
+        body{ font: 14px sans-serif;  background-image: radial-gradient(lightblue, grey); }
+        .wrapper{ width: 350px; padding: 20px;  text-align: center;}
     </style>
 </head>
 <body>
