77e7852be10cae89359ee5b844231c464803d029
Shivanireddy25 <shivanireddy.singireddy@sjsu.edu>
2019-05-14 19:12:30 -0700

    Toprate

--- html/TopRateAndReview.php
+++ html/TopRateAndReview.php
@@ -1,6 +1,6 @@
 <?php
-// (1) Include rating library and DB connection
-	require_once "configuration.php";
+
+	require_once "config.php";
 	require_once "rating_lib.php";
 
 	$starLib = new Rating();
