81b9c14120d460aec1f760f1849dbe715fcc588a
Shivanireddy25 <shivanireddy.singireddy@sjsu.edu>
2019-05-18 21:22:46 -0700

    Signup error page

--- html/registration.php
+++ html/registration.php
@@ -152,7 +152,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
-                header("location: home.html");
+                header("location: home.php");
             } else{
                 echo "Something went wrong. Please try again later.";
 				printf("Error: %s.\n", mysqli_stmt_error($stmt));
